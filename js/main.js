276c5804ed73098e5dc683f43fef8ba372d01d54
MohamedAboSeada <madoaxel287@gmail.com>
2024-05-21 10:40:25 +0300

    make bills object that saved the choosed plan and addons and make some changes in design to make some changes in desgin

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const nextBtn = document.getElementById('next');
 	const lastBtn = document.getElementById('last');
-
+	let bills = {
+		plan: document.querySelector('.plans_cards div.active').children[1]
+			.children[1].textContent,
+		services: [...document.querySelectorAll('addon__card.active .price')],
+	};
 	nextBtn.addEventListener('click', () => {
 		const activeStep = document.querySelector('p.active');
 		const activeForm = document.querySelector('.show');
@@ -50,15 +54,37 @@ document.addEventListener('DOMContentLoaded', () => {
 	// choose plan
 	document.querySelector('.plans_cards').addEventListener('click', (e) => {
 		let choosedPlan = document.querySelector('.plans_cards div.active');
-        choosedPlan.classList.remove('active');
+		choosedPlan.classList.remove('active');
 		if (e.target.tagName === 'DIV') {
 			if (e.target.classList.contains('plan_card')) {
 				e.target.classList.add('active');
+				bills.plan = e.target.children[1].children[1].textContent;
 			} else {
 				e.target.parentNode.classList.add('active');
+				bills.plan =
+					e.target.parentNode.children[1].children[1].textContent;
 			}
 		} else {
 			e.target.parentNode.parentNode.classList.add('active');
+			bills.plan =
+				e.target.parentNode.parentNode.children[1].children[1].textContent;
+		}
+	});
+
+	// make checkbox choose
+	document.querySelector('.addons__cards').addEventListener('click', (e) => {
+		if (e.target.tagName === 'INPUT') {
+			if (!e.target.checked) {
+				e.target.parentNode.parentNode.classList.remove('active');
+				bills.services = [
+					...document.querySelectorAll('.active .price'),
+				];
+			} else {
+				e.target.parentNode.parentNode.classList.add('active');
+				bills.services = [
+					...document.querySelectorAll('.active .price'),
+				];
+			}
 		}
 	});
 });
